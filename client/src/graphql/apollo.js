709bebb401860d60ce7544d35a75af50fbfe5c3e
Du Philip <philip_du_an@hotmail.com>
2020-06-06 20:14:11 -0700

    Set up client

---
+++ client/src/graphql/apollo.js
@@ -0,0 +1,10 @@
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new createHttpLink({
+    uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
+  }),
+});
+
+export default client;
---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import { ApolloProvider } from "@apollo/client";
+import { Grommet } from "grommet";
+import React from "react";
+import ReactDOM from "react-dom";
+
+import client from "./graphql/apollo";
+import GlobalStyle from "./styles/global";
+import theme from "./styles/theme";
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <GlobalStyle />
+    <Grommet theme={theme}>
+      <div>
+        <p>Hello World!</p>
+      </div>
+    </Grommet>
+  </ApolloProvider>
+);
+ReactDOM.render(<App />, document.getElementById("root"));
