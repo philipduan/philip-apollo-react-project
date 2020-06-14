eb5f974962bbd18e56f5f53a15831324ac93b4e4
Du Philip <philip_du_an@hotmail.com>
2020-06-14 13:27:01 -0700

    Temp

---
+++ client/src/components/ProfileListItem/index.js
@@ -0,0 +1,11 @@
+import { Anchor, Box, Button, Image, Text } from "grommet";
+import { Link } from "react-router-dom";
+import React from "react";
+
+const ProfileListItem = ({ profileData }) => {
+  const { avatar, description, fullName, username } = profileData;
+
+  return <Box></Box>;
+};
+
+export default ProfileListItem;
